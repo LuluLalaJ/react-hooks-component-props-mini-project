57cb77ad04ac4419ff8f77b13c0879c000978d38
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-12 10:09:28 -0400

    create empty components

---
+++ src/components/Article.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Article() {
+    return (
+        <div>
+            Hello from Article
+        </div>
+    )
+}
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ArticleList() {
+    return (
+        <div>
+            Hello from ArticleList
+        </div>
+    )
+}
+
+export default ArticleList;
