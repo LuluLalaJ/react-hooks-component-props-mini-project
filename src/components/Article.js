c7ac2f2a4f244f2601a54ffa4394a431772b77a6
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-17 15:54:25 -0400

    add emojis

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,12 +2,25 @@ import React from "react";
 
 function Article(post) {
     const {title, date = "January 1, 1970" , preview, minutes} = post
+    
+    function createEmojis(minutes) {
+        const interval = minutes < 30 ? 5 : 10
+        const emoji = minutes < 30 ? "☕️" : "🍱"
+
+        let emojis = ""
+        for ( let i = 0; i < minutes; i += interval) {
+            emojis += emoji
+        } 
+        return emojis 
+    }
+
+    const displayEmojis = createEmojis(minutes)
 
     return (
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
-            {/* <span>{displayMinutes}</span> */}
+            <span> | {displayEmojis} {minutes} minutes read</span>
             <p>{preview}</p>
         </article>
     )
